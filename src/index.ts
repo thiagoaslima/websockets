1457a0f809b282d596c39187f013127ba229c468
Thiago Lima <thiago@growthloop.com>
2023-10-20 12:01:18 -0600

    correct host

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 import {server} from './server.js';
 import {extendServerWithWebsocket} from './websocket_server.js';
-const PORT = process.env.PORT || 1337;
+const PORT = 1337;
 extendServerWithWebsocket(server);
 server.listen({port: PORT, host: '0.0.0.0'}, (err, address) => {
   if (err) throw err;
