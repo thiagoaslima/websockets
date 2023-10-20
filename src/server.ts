7692bf1db8871c59430da9d5177c6c6c21eb30ea
Thiago Lima <thiago@growthloop.com>
2023-10-20 11:54:22 -0600

    cors

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,7 @@
-import path from 'node:path';
 import fastify from 'fastify';
 import websocket from '@fastify/websocket';
 import fastifyStatic from '@fastify/static';
+import fastifyCors from 'fastify-cors';
 
 import routes from './routes.js';
 import type {TypeBoxTypeProvider} from '@fastify/type-provider-typebox';
@@ -9,6 +9,11 @@ import type {TypeBoxTypeProvider} from '@fastify/type-provider-typebox';
 export const server = fastify({
   logger: true,
 }).withTypeProvider<TypeBoxTypeProvider>();
+
+server.register(fastifyCors, {
+  origin: '*',
+  logLevel: 'warn',
+});
 
 server.register(fastifyStatic, {
   root: new URL('static', import.meta.url),
